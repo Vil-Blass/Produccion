0abedc65cf9dae90a91525c55b53b53bfb575e5f
Vil-Blass <jeffry16snake@hotmail.com>
2024-03-21 10:52:00 +0100

    get para el form

--- sueños.js
+++ sueños.js
@@ -1,6 +1,7 @@
-
-//validar nombre de socio
+// Definir la variable inputNombre
 let inputNombre = document.querySelector("#Nombre");
+
+// Validar nombre de socio
 inputNombre.addEventListener("input", (event) => {
     if (event.target.value.length > 0) {
         inputNombre.classList.add("is-valid");
@@ -11,9 +12,8 @@ inputNombre.addEventListener("input", (event) => {
     }
 });
 
-//validar fecha
+// Validar fecha
 let inputFecha = document.querySelector("#fecha");
-
 inputFecha.addEventListener("input", (event) => {
     if (event.target.value.length > 0) {
         inputFecha.classList.add("is-valid");
@@ -22,11 +22,9 @@ inputFecha.addEventListener("input", (event) => {
         inputFecha.classList.add("is-invalid");
         inputFecha.classList.remove("is-valid");
     }
-})
+});
 
-
-
-//validar cantidad
+// Validar cantidad
 let inputCantidad = document.querySelector("#cantidad");
 inputCantidad.addEventListener("input", (event) => {
     if (event.target.value.length > 0) {
@@ -36,29 +34,29 @@ inputCantidad.addEventListener("input", (event) => {
         inputCantidad.classList.add("is-invalid");
         inputCantidad.classList.remove("is-valid");
     }
-})
+});
 
-//seleccionar tipo de produccion
+// Seleccionar tipo de producción
 let selectTipoU = document.querySelector("#selector");
-  selectTipoU.addEventListener("change", (event) => {
-            const selectedValue = event.target.value;
-            console.log("Seleccionaste: " + selectedValue);
-        });
+selectTipoU.addEventListener("change", (event) => {
+    const selectedValue = event.target.value;
+    console.log("Seleccionaste: " + selectedValue);
+});
 
-//validar enviar
+// Validar enviar
 let inputEnviar = document.querySelector("#enviar");
 inputEnviar.addEventListener("click", (event) => {
     event.preventDefault();
-    if (inputNombre.classList.contains("is-valid") && inputFecha.classList.contains("is-valid")&& inputCantidad.classList.contains("is-valid") && selectTipoU.value != "") {
-        alert("enviado");
+    if (inputNombre.classList.contains("is-valid") && inputFecha.classList.contains("is-valid") && inputCantidad.value != "" && selectTipoU.value != "") {
+        alert("Enviado");
         return true;
     }
-    //reiniciar campos
+    // Reiniciar campos
     inputNombre.value = "";
     inputFecha.value = "";
     inputCantidad.value = "";
     selectTipoU.value = "";
-    })
+});
 
 // Guardar datos en localStorage
 localStorage.setItem('nombre', inputNombre.value);
@@ -81,12 +79,9 @@ if (fechaGuardada) {
     console.log('Fecha guardada: ' + fechaGuardada);
 }
 
-
-
 if (cantidadGuardada) {
     console.log('Cantidad guardada: ' + cantidadGuardada + selectTipoU.value);
 }
-
 
 // No es necesario acceder a .value ya que los valores ya están guardados en localStorage
 console.log(nombreGuardado);
